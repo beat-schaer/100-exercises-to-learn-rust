5d180304713b321f6fee3d300fc3bcda4a855bce
Beat Schär <beat@schaers.net>
2025-05-25 11:54:37 +0200

    Implemented chapter 6 part 1

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,11 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fibo: Vec<u32> = vec![0, 1];
+    for i in 2..=n {
+        fibo.push(fibo[(i-2) as usize] + fibo[(i-1) as usize]);
+    }
+    fibo[n as usize]
 }
 
 #[cfg(test)]
